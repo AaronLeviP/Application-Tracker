7095ef6bb0148260e5a4b1e8e781d1e3572265be
Aaron Purnawan <AaronLeviP@outlook.com>
2026-02-25 19:50:06 -0500

    Tokens are auto added onto applicationAPI endpoints and expired tokens are handled by redirecting to login page

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -9,6 +9,38 @@ const api = axios.create({
     }
 });
 
+// Automatically add token to every request
+api.interceptors.request.use (
+    (config) => {
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// Handle 401 errors (token expired/invalid)
+api.interceptors.response.use (
+    (response) =>  response,
+    (error) => {
+        if(error.response?.staatus === 401) {
+            // Token is invalid - clear it
+            localStorage.removeItem('token');
+
+            // Redirect to login
+            window.location.href = '/login';
+        }
+
+        return Promise.reject(error);
+    }
+)   
+
 export const applicationAPI = {
     getAll: () => api.get('/applications'),
     create: (data) => api.post('/applications', data),
